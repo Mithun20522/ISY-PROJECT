eabc57fb7755cd675f1694f55407c8e5647785c5
mithun20522 <mithun20522@iiitd.ac.in>
2024-04-14 13:47:35 +0530

    user authentication done.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,24 +1,69 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import toast from 'react-hot-toast';
+import {Popup} from 'reactjs-popup';
+import { logoutSuccess } from '../redux/user/userSlice';
 const Header = () => {
   const [clicked, setClicked] = useState(false);
+  const [userLogout, setUserLogout] = useState(false);
   const dispatch = useDispatch();
   const {currentUser} = useSelector((state) => state.user);
-  console.log(currentUser);
+  const handleSubmit = async() => {
+    try {
+      const res = await fetch('http://localhost:3000/api/auth/signout',{
+        method:'POST'
+      });
+      const data = await res.json();
+      if(res.ok){
+        dispatch(logoutSuccess());
+        toast.success(data.message);
+        return;
+      }
+      else{
+        console.log(data.message);
+        toast.error(data.message);
+        return;
+      }
+    } catch (error) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  }
+
   return (
-    <nav className='p-7 flex items-center justify-between bg-slate-300 shadow-md border-b-2 border-black'>
+    <header className='p-7 flex items-center justify-between bg-slate-300 shadow-md border-b-2 border-black'>
         <h1 className='cursor-pointer'>CollegeBuddy</h1>
         <AiOutlineMenu className='sm:hidden' onClick={() => setClicked(!clicked)}/>
-        <ul className='hidden sm:flex gap-5 items-center'>
+        <ul className='hidden sm:flex gap-5 items-center mx-24'>
                 <Link className='hover:scale-75 transition-all duration-50 ease-out' to={'/'}><li>Home</li></Link>
                 <Link className='hover:scale-75 transition-all duration-50 ease-out' to={'/peer-connect'}><li>Peer connect</li></Link>
                 <Link className='hover:scale-75 transition-all duration-50 ease-out' to={'/resources'}><li>Resources</li></Link>
                 <Link className='hover:scale-75 transition-all duration-50 ease-out' to={'/well-being'}><li>Well being cell</li></Link>
-                <Link to={'/login'} className='px-3 py-1 rounded-md bg-slate-700 hover:bg-slate-900 text-white'>Login</Link>
+                {
+                  currentUser ? (
+                      <div className=''>
+                        <img src={currentUser.rest.avatar} onClick={() => setUserLogout(!userLogout)} alt={currentUser.rest.username} className='w-10 h-10 cursor-pointer rounded-full'/>
+                         {
+                          userLogout ? (
+                            <div className='bg-sky-300 absolute px-12 py-3 right-[7%] space-y-5 rounded-lg shadow-lg items-center'>
+                              <p className='text-xl'>Are you sure you want to logout ? </p>
+                              <div className='flex justify-between'>
+                              <button className='bg-slate-500 px-3 text-sm rounded-md hover:bg-slate-700 font-medium text-white' onClick={() => setUserLogout(!userLogout)}>No</button>
+                              <button className='bg-red-500 p-2 text-sm rounded-md hover:bg-red-700 font-medium text-white' onClick={handleSubmit}>Log out</button>
+                              </div>
+                            </div>
+                          ): ""
+                         }
+                         <p className='text-lg font-bold items-center '>{currentUser.rest.username}</p>
+                      </div>
+                  ): (
+                    <Link to={'/login'} className='px-3 py-1 rounded-md bg-slate-700 hover:bg-slate-900 text-white'>Login</Link>
+                  )
+                }
             </ul>
-    </nav>
+    </header>
   )
 }
 
